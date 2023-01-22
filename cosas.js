88b91ec3df46dd271bcf93505eb28256365b9194
DiegoSorribas <123317834+DiegoSorribas@users.noreply.github.com>
2023-01-22 14:29:27 -0300

    Add files via upload

---
+++ cosas.js
@@ -0,0 +1,16 @@
+
+document.getElementById("Dire").addEventListener("click", function() {
+    document.getElementById("contenedor").innerHTML = "Pasaje Alcántara 5563, Abra Pampa, Jujuy";
+});
+
+document.getElementById("Fono").addEventListener("click", function() {
+    document.getElementById("contenedor").innerHTML = "+54 011 1234-5678";
+});
+
+document.getElementById("Mail").addEventListener("click", function() {
+    document.getElementById("contenedor").innerHTML = "r.perfumo@chimail.com";
+});
+
+document.getElementById("Nací").addEventListener("click", function() {
+    document.getElementById("contenedor").innerHTML = "06 de Noviembre de 2084";
+});
