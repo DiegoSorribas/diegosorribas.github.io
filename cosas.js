17cc424378d0d82e2082c04058df1e83178ebdf6
DiegoSorribas <123317834+DiegoSorribas@users.noreply.github.com>
2023-01-23 12:17:19 -0300

    Add files via upload

--- cosas.js
+++ cosas.js
@@ -1,16 +1,20 @@
+document.getElementById("Nací").addEventListener("click", function() {
+    document.getElementById("contenedor").innerHTML = "06 de Noviembre de 2084";
+});
+
+document.getElementById("Nacio").addEventListener("click", function() {
+    document.getElementById("contenedor").innerHTML = "Argentina";
+});
 
 document.getElementById("Dire").addEventListener("click", function() {
     document.getElementById("contenedor").innerHTML = "Pasaje Alcántara 5563, Abra Pampa, Jujuy";
 });
 
-document.getElementById("Fono").addEventListener("click", function() {
-    document.getElementById("contenedor").innerHTML = "+54 011 1234-5678";
+document.getElementById("Cuil").addEventListener("click", function() {
+    document.getElementById("contenedor").innerHTML = "99-123456789-0";
 });
 
-document.getElementById("Mail").addEventListener("click", function() {
-    document.getElementById("contenedor").innerHTML = "r.perfumo@chimail.com";
+document.getElementById("Linked").addEventListener("click", function() {
+    document.getElementById("contenedor").innerHTML = "linkedin.com/in/roberto-perfumo-005bb50b (enlace ficticio)";
 });
 
-document.getElementById("Nací").addEventListener("click", function() {
-    document.getElementById("contenedor").innerHTML = "06 de Noviembre de 2084";
-});
